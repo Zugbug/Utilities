0e541036c2b40d4aae3b439f7b90f75213b331eb
Zugbug <kirilos24@gmail.com>
2016-08-30 23:53:53 +0100

    Full javadoc of the Chainable Runnable class

--- src/utils/ChainableRunnable.java
+++ src/utils/ChainableRunnable.java
@@ -1,21 +1,30 @@
 package utils;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
+ * ChainableRunnable provides pseudo-"syntactic sugar" for chanining multiple
+ * runnables. There's a way to construct them functionally and parametrically.
  *
  * @author zugbug
  */
 public interface ChainableRunnable extends Runnable {
 
+    /**
+     * Empty Chainable, to be used as a starting point for a chain.
+     *
+     * @return a runnable that does nothing
+     */
     public static ChainableRunnable empty() {
         return () -> {
         };
     }
 
+    /**
+     * Builder method, provided with an array of runnables produces a single
+     * chainable. First element in the array will be executed first.
+     *
+     * @param rs an array of runnables
+     * @return a single runnable
+     */
     public static ChainableRunnable build(Runnable... rs) {
         ChainableRunnable ret = empty();
         for (Runnable r : rs) {
@@ -24,6 +33,13 @@ public interface ChainableRunnable extends Runnable {
         return ret;
     }
 
+    /**
+     * Appends the instructions of the target runnable to this one.
+     *
+     * @param r the next instruction
+     * @return a new Runnable where first this object's instructions are
+     * executes and then the target one's
+     */
     default ChainableRunnable andThen(Runnable r) {
         if (r == null) {
             return this::run;
