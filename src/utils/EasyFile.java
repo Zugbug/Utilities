6551930197d8e341b24b72d61d4c33b0c64198a0
Zugbug <kirilos24@gmail.com>
2016-08-30 23:52:22 +0100

    Moved Throwing* utilities into their own file

--- src/utils/EasyFile.java
+++ src/utils/EasyFile.java
@@ -8,16 +8,13 @@ package utils;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.OutputStream;
 import java.util.Scanner;
-import java.util.function.BiFunction;
-import java.util.function.Consumer;
 import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
+import utils.Throwing.Consumer;
+import utils.Throwing.Supplier;
 
 /**
  *
@@ -25,111 +22,14 @@ import java.util.stream.StreamSupport;
  */
 public class EasyFile {
 
-    interface OutputLambdaStream {
-
-        void write(int b);
-
-        default OutputStream get() {
-            return new OutputStream() {
-                @Override
-                public void write(int b) throws IOException {
-                    OutputLambdaStream.this.write(b);
-                }
-            };
-        }
-    }
-
-    @FunctionalInterface
-    public interface ThrowingSupplier<R> extends Supplier<R> {
-
-        @Override
-        default public R get() {
-            try {
-                return getThrows();
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        R getThrows() throws Exception;
-
-    }
-
-    @FunctionalInterface
-    public interface ThrowingConsumer<T> extends Consumer<T> {
-
-        @Override
-        default public void accept(T t) {
-            try {
-                acceptThrows(t);
-            } catch (Exception e) {
-                throw new RuntimeException("Could not consume " + t + ": " + e.getMessage());
-            }
-        }
-
-        void acceptThrows(T val) throws Exception;
-
-    }
-
-    @FunctionalInterface
-    public interface ThrowingBiFunction<X, Y, Z> extends BiFunction<X, Y, Z> {
-
-        @Override
-        default public Z apply(X t, Y u) {
-            try {
-                return applyThrows(t, u);
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        Z applyThrows(X t, Y u) throws Exception;
-
-    }
-
-    @FunctionalInterface
-    public interface ThrowingPredicate<T> extends Predicate<T> {
-
-        @Override
-        default public boolean test(T t) {
-            try {
-                return testThrows(t);
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        public boolean testThrows(T t) throws Exception;
-
-    }
-
-    /**
-     * @see Function
-     * @param <T>
-     * @param <R>
-     */
-    @FunctionalInterface
-    public interface ThrowingFunction<T, R> extends Function<T, R> {
-
-        @Override
-        default R apply(final T elem) {
-            try {
-                return applyThrows(elem);
-            } catch (final Exception e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        R applyThrows(T elem) throws Exception;
-    }
-
+  
     public static String read(File file) throws FileNotFoundException {
         return stream(new Scanner(file).useDelimiter("\n"))
-                .reduce((a, b) -> a  +"\n"+ b).orElse("");
+                .reduce((a, b) -> a + "\n" + b).orElse("");
     }
 
     public static Stream<String> stream(Scanner scanner) {
-        return StreamSupport.stream(((Iterable<String>) ((ThrowingSupplier<Scanner>) () -> scanner)::get)
+        return StreamSupport.stream(((Iterable<String>) ((Supplier<Scanner>) () -> scanner)::get)
                 .spliterator(), false);
     }
 
@@ -154,7 +54,7 @@ public class EasyFile {
     }
 
     public static void printStream(OutputStream out, String msg) {
-        msg.chars().boxed().map(s -> (char) s.intValue()).forEach((ThrowingConsumer<Character>) out::write);
+        msg.chars().boxed().map(s -> (char) s.intValue()).forEach((Consumer<Character>) out::write);
     }
 
 }
---
+++ src/utils/Throwing.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+/**
+ *
+ * @author zugbug
+ */
+public class Throwing {
+
+    /**
+     * @see Function
+     * @param <T>
+     * @param <R>
+     */
+    @FunctionalInterface
+    public interface Function<T, R> extends java.util.function.Function<T, R> {
+
+        @Override
+        default R apply(final T elem) {
+            try {
+                return applyThrows(elem);
+            } catch (final Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        R applyThrows(T elem) throws Exception;
+    }
+
+    @FunctionalInterface
+    public interface Predicate<T> extends java.util.function.Predicate<T> {
+
+        @Override
+        default public boolean test(T t) {
+            try {
+                return testThrows(t);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        public boolean testThrows(T t) throws Exception;
+
+    }
+
+    @FunctionalInterface
+    public interface BiFunction<X, Y, Z> extends java.util.function.BiFunction<X, Y, Z> {
+
+        @Override
+        default public Z apply(X t, Y u) {
+            try {
+                return applyThrows(t, u);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        Z applyThrows(X t, Y u) throws Exception;
+
+    }
+
+    @FunctionalInterface
+    public interface Supplier<R> extends java.util.function.Supplier<R> {
+
+        @Override
+        default public R get() {
+            try {
+                return getThrows();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        R getThrows() throws Exception;
+
+    }
+
+    @FunctionalInterface
+    public interface Consumer<T> extends java.util.function.Consumer<T> {
+
+        @Override
+        default public void accept(T t) {
+            try {
+                acceptThrows(t);
+            } catch (Exception e) {
+                throw new RuntimeException("Could not consume " + t + ": " + e.getMessage());
+            }
+        }
+
+        void acceptThrows(T val) throws Exception;
+
+    }
+}
